267d77e52ca606591283605ba55d9246b1b4a34a
apriadi4425 <apripeni15@gmail.com>
2021-03-25 16:33:13 +0800

    memperbaiki sedikit agar lebih rapi di route index

--- route/index.js
+++ route/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router();
+const { authenticateToken } = require('../midleware')
 
 const users = require('./user')
+const shopping = require('./shopping')
 
 router.use('/users', users);
+router.use('/shopping', authenticateToken, shopping);
 
 module.exports = router;
--- route/shopping.js
+++ route/shopping.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const router = express.Router();
 
-const { authenticateToken } = require('../midleware')
 
 const ShoppingController = require('../controller/ShoppingController')
 
-router.get('/', authenticateToken, ShoppingController.getAll);
-router.post('/', authenticateToken, ShoppingController.Create);
-router.post('/:id', authenticateToken, ShoppingController.getById);
-router.put('/:id', authenticateToken, ShoppingController.Update);
-router.delete('/:id', authenticateToken, ShoppingController.delete);
+router.get('/', ShoppingController.getAll);
+router.post('/', ShoppingController.Create);
+router.post('/:id', ShoppingController.getById);
+router.put('/:id', ShoppingController.Update);
+router.delete('/:id', ShoppingController.delete);
 
 
 module.exports = router;
